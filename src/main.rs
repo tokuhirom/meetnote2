e857c6c321967166653bdcacc846c5749262356c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-12-01 18:39:07 +0900

    move some functions

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,10 @@
 use std::thread::sleep;
 use std::time::Duration;
-use screencapturekit::sc_shareable_content::SCShareableContent;
 use chrono::Local;
-use cpal::InputDevices;
 use cpal::traits::{DeviceTrait, HostTrait}; // for timestamp
 
 mod audio;
-
-pub struct WindowPattern {
-    bundle_id: String,
-    window_title: String,
-}
-
-// TOOD make this configurable
-fn get_window_patterns() -> Vec<WindowPattern> {
-    let mut result = Vec::new();
-    result.push(
-        WindowPattern {
-            bundle_id: String::from("us.zoom.xos"),
-            window_title: String::from("Zoom Meeting"),
-        }
-    );
-    result.push(
-        WindowPattern {
-            bundle_id: String::from("us.zoom.xos"),
-            window_title: String::from("zoom share toolbar window"),
-        }
-    );
-    result.push(
-        WindowPattern {
-            bundle_id: String::from("us.zoom.xos"),
-            window_title: String::from("zoom share statusbar window"),
-        }
-    );
-    return result
-}
-
-
-fn is_there_target_windows() -> bool {
-    let current = SCShareableContent::current();
-
-    let patterns = get_window_patterns();
-
-    for window in current.windows {
-        if let Some(title) = window.title {
-            if let Some(app) = window.owning_application {
-                if let Some(bundle_id) = app.bundle_identifier {
-                    for pattern in &patterns {
-                        if (pattern.bundle_id == bundle_id && pattern.window_title == title) {
-                            return true
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return false
-}
+mod window;
 
 fn main() {
     // #1 Check if the platform is supported
@@ -96,7 +44,7 @@ fn main() {
     }
 
     loop {
-        if is_there_target_windows() {
+        if window::is_there_target_windows() {
             if !is_recording {
                 is_recording = true;
 
---
+++ src/window.rs
@@ -0,0 +1,52 @@
+use screencapturekit::sc_shareable_content::SCShareableContent;
+
+pub struct WindowPattern {
+    bundle_id: String,
+    window_title: String,
+}
+
+// TOOD make this configurable
+fn get_window_patterns() -> Vec<WindowPattern> {
+    let mut result = Vec::new();
+    result.push(
+        WindowPattern {
+            bundle_id: String::from("us.zoom.xos"),
+            window_title: String::from("Zoom Meeting"),
+        }
+    );
+    result.push(
+        WindowPattern {
+            bundle_id: String::from("us.zoom.xos"),
+            window_title: String::from("zoom share toolbar window"),
+        }
+    );
+    result.push(
+        WindowPattern {
+            bundle_id: String::from("us.zoom.xos"),
+            window_title: String::from("zoom share statusbar window"),
+        }
+    );
+    return result
+}
+
+
+pub fn is_there_target_windows() -> bool {
+    let current = SCShareableContent::current();
+
+    let patterns = get_window_patterns();
+
+    for window in current.windows {
+        if let Some(title) = window.title {
+            if let Some(app) = window.owning_application {
+                if let Some(bundle_id) = app.bundle_identifier {
+                    for pattern in &patterns {
+                        if (pattern.bundle_id == bundle_id && pattern.window_title == title) {
+                            return true
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false
+}
