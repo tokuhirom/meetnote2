25ca80ff8b525634dc8421ec99576c3dde3b0fef
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-12-04 22:49:00 +0900

    Do not update textarea while editing...

--- src/lib/FileItem.svelte
+++ src/lib/FileItem.svelte
@@ -7,6 +7,7 @@
     };
 
     let editMode = false;
+    let editingContent : string | null = null;
 
     async function deleteItem(filename: string) {
         console.log(`Delete file: ${filename}`)
@@ -14,9 +15,18 @@
         return true;
     }
 
+    async function enterEditingMode() {
+        editMode = true
+        editingContent = file.content
+    }
+
     async function saveItem() {
         console.log(`Save file: ${file.filename}`)
-        await invoke("save_file", file);
+        await invoke("save_file", {
+            filename: file.filename,
+            content: editingContent
+        });
+        file.content = editingContent
         editMode = false;
     }
 </script>
@@ -25,13 +35,13 @@
     <div>
         <h2 style="float: left;">{file.filename}</h2>
         <div style="float: right">
-            <button on:click={e => editMode = true}>Edit</button>
+            <button on:click|preventDefault={enterEditingMode}>Edit</button>
             <button on:click={e => deleteItem(file.filename)}>Delete</button>
         </div>
     </div>
     {#if editMode}
         <form on:submit|preventDefault={saveItem}>
-            <textarea cols="80" rows="40" bind:value={file.content}></textarea>
+            <textarea cols="80" rows="40" bind:value={editingContent}></textarea>
             <button type="submit">Save</button>
         </form>
     {:else}
