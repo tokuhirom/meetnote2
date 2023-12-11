be2e50482c215eb55c6b5d657daab2220b696f01
Tokuhiro Matsuno <tokuhirom@linecorp.com>
2023-12-11 09:43:17 +0900

    Show window patterns

--- src/Configuration.svelte
+++ src/Configuration.svelte
@@ -3,9 +3,14 @@
   import {onMount} from "svelte";
   import {getCurrent} from "@tauri-apps/api/window";
 
-  let config = {
+  let config : {
+    openai_api_token: string | undefined,
+    target_device: string | undefined,
+    window_patterns: {bundle_id: string, window_title: string}[]
+  } = {
     openai_api_token: undefined,
-    target_device: undefined
+    target_device: undefined,
+    window_patterns: []
   };
   let devices:  string[] = [];
 
@@ -25,17 +30,35 @@
   <h1>Configuration - MeetNote2</h1>
 
   <form on:submit|preventDefault={saveConfig}>
-    <div>
-      OpenAI API Key:
-      <input type="text" bind:value={config.openai_api_token}>
+    <div class="pane">
+      <h3>Basic configuration</h3>
+      <div>
+        OpenAI API Key:
+        <input type="text" bind:value={config.openai_api_token}>
+      </div>
+      <div>
+        Target device:
+        <select bind:value={config.target_device}>
+          {#each devices as device}
+            <option value={device}>{device}</option>
+          {/each}
+        </select>
+      </div>
     </div>
-    <div>
-      Target device:
-      <select bind:value={config.target_device}>
-        {#each devices as device}
-          <option value={device}>{device}</option>
-        {/each}
-      </select>
+    <div class="pane">
+      <h3>Window patterns</h3>
+      <table>
+        <tr>
+          <th>Bundle ID</th>
+          <th>Window title</th>
+        </tr>
+      {#each config.window_patterns as pattern}
+        <tr>
+          <td>{pattern.bundle_id}</td>
+          <td>{pattern.window_title}</td>
+        </tr>
+      {/each}
+      </table>
     </div>
     <button type="submit">Save</button>
   </form>
@@ -43,4 +66,11 @@
 </main>
 
 <style>
+  .pane {
+    margin-bottom: 40px;
+  }
+  td, th {
+    border-right: 1px solid white;
+    padding: 4px;
+  }
 </style>
