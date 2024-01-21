ed977e4d2eeb5fd97edcadae83a33bbc4924da4c
Tokuhiro Matsuno <tokuhirom@linecorp.com>
2024-01-21 22:49:32 +0900

    ignore .DS_Store

--- src/lib/data_repo.ts
+++ src/lib/data_repo.ts
@@ -20,6 +20,10 @@ export class DataRepo {
         const result: Entry[] = [];
         const entries = await readDir('data', { dir: BaseDirectory.AppData, recursive: false });
         for (const fileEntry of entries) {
+            if (fileEntry.path.endsWith(".DS_Store")) { // Ignore .DS_Store
+                continue;
+            }
+
             if (await exists(fileEntry.path)) {
                 let entry = await Entry.fromPath(fileEntry.path);
                 result.push(entry);
